3cb77d53a6013b7ca3c7fd0156faed52b76b31a3
Janet Martin <akamimartin@gmail.com>
2015-05-19 18:07:56 -0700

    Janet Martin finished Prototype-ajax

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,13 +1,38 @@
-$(document).ready(function(){
-	$('button').click(function(){
-		console.log('click initiated');
-		$.ajax({
-			dataType: 'json',
-			url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
-			success: function(result){
-				console.log('loaded',result);
+var global_response
 
-			}
-		});
-	});
+$(document).ready(function() {
+    $('button').click(function() {
+        console.log('click initiated');
+
+        $.ajax({
+            dataType: 'json',
+            url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
+            success: function(result) {
+
+
+                window.result = result;
+                global_response = result.feed.entry;
+                for (var i = 0; i < 10; i++) {
+                                    
+
+                    var movies = result.feed.entry;
+                    var movies_image_url =  movies[i]['im:image'][2].label;
+                    var title = movies[i].title.label;
+                    
+                    var container = $('<figure>');
+                    
+                    var title = $("<figcaption>").html(title); 
+                                       
+                    var movie_image = $('<img>').attr('src', movies_image_url);
+                    console.log("movies:" , movies_image_url);
+                     $(container).append(movie_image,title);
+                     $('#main').append(container);
+
+                  
+                };
+            } 
+
+        });
+    });
 });
+ 
