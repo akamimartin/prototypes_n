3996ebc711a3863e209687b47c9f8a59d2d18d15
Janet Martin <akamimartin@gmail.com>
2015-05-26 16:43:21 -0700

    changes

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -11,23 +11,26 @@ $(document).ready(function() {
 
 
                 window.result = result;
-                global_response = result.feed.entry;
+                global_response = result.data.data;
                 for (var i = 0; i < 10; i++) {
+                    console.log(
                                     
-
-                    var movies = result.feed.entry;
+               /*
+                    var students = result.data.data[0].name;
                     var movies_image_url =  movies[i]['im:image'][2].label;
                     var title = movies[i].title.label;
                     
                     var container = $('<figure>');
                     
                     var title = $("<figcaption>").html(title); 
-                                       
+                     var div = $('<div class = "movie_info">) 
+                                
                     var movie_image = $('<img>').attr('src', movies_image_url);
                     console.log("movies:" , movies_image_url);
+                    
                      $(container).append(movie_image,title);
                      $('#main').append(container);
-
+                  */
                   
                 };
             } 
