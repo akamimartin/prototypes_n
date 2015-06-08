47f76a39ab6ba3d12dfec6850d89acee5a450121
Janet Martin <akamimartin@gmail.com>
2015-06-08 14:57:20 -0700

    latest changes

--- php_regex/receiver.php
+++ php_regex/receiver.php
@@ -1,2 +1,60 @@
 <?php
+
+ 
+
+ <!doctype html>
+<html>
+<body>
+<form action="receiver.php" method="post">
+<input type="text" name="first_name" placeholder="first_name"><br>
+$first_name;
+$age;
+$phone;
+$username;
+$bonus;
+$submit;
+
+ 
+
+echo ($_POST['$first_name']);
+echo ($_POST['$age']);
+echo ($_POST['$username']);
+echo ($_POST['$bonus']);
+echo ($_POST['$submit']);
+
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+
+ /*for number two*/
+//$person = ['name' => 'Janet', 'age' ==> 2],
+//foreach($person as $key => $value}
+//{
+
+//  print("key=$key:value=$value)
+
+//}) 
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
